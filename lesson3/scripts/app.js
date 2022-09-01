e97832d915edeb4643af649f18cce42bec10bb12
Abidemi Ope <opeabidemi@gmail.com>
2022-09-01 13:59:18 +0100

    new lesson

---
+++ lesson3/scripts/app.js
@@ -0,0 +1,48 @@
+// // Data Types
+
+// let country = "NIGERIA IS A BAD COUNTRY";
+// let sentence  = "I am 12 years old"
+// let population = 30000.4545454545;
+// let isPoor = true;
+
+// // Strings
+// console.log(country.concat(" Africa"))
+// console.log(country.padStart(20, "KA"));
+// console.log(country.padEnd(20, "I"));
+// console.log(country.replaceAll(" ", "-"));
+
+// // console.log(typeof country);
+
+// // Numbers
+
+// console.log(population.toFixed(3));
+// console.log(population.toExponential());
+// console.log(population.toPrecision(4));
+// console.log(population.toString());
+
+// console.log(population + 10);
+
+// // 
+
+// let age1 = "20";
+// let age2 = 20;
+
+// console.log(age1 + 2);
+// console.log(age2 + 2);
+
+// let name = prompt("Enter your name");
+// let year = prompt("Enter the year you were born");
+
+// console.log(name);
+// console.log(parseFloat(year));
+
+// let a  = prompt("Enter first number")
+// let b = prompt("Enter second number")
+
+// console.log(parseFloat(a)  + parseFloat(b));
+
+
+// Boolean
+let isDark = false;
+
+console.log(parseInt(isDark));
---
+++ lesson3/scripts/operators.js
@@ -0,0 +1,54 @@
+// Operators
+
+// Assignment Operators
+// Arithmetic Operators
+// Logical 
+// Relational
+// Tenary
+
+
+// // Assignment
+// let age = 20;
+
+// // console.log(age);
+
+// age += 10; // 30
+// age *= 10; // 300
+// age -= 10; // 290
+// age /= 10; // 29
+// age %= 10; // 9
+
+// console.log(age);
+
+
+// // Arithmetic
+let num = 40;
+let num2 = 20;
+
+// console.log(num + num2); // 60
+// console.log(num * num2); // 800
+// console.log(num / num2); // 2
+// console.log(num % num2); // 0
+
+// Post
+// console.log(num++); // 40
+// console.log(num); // 41
+// console.log(num--); // 41
+// console.log(num); // 40
+
+// // Pre
+// console.log(++num); // 41
+// console.log(num); // 41
+// console.log(--num); // 40
+// console.log(num); // 40
+
+// num = 40, num2 = 20;
+// console.log((--num)++);
+
+// num **= 2; // num = num ** 2
+
+// console.log(!false);
+
+// console.log(5 ^ 2);
+// console.log(10 << 4);
+
